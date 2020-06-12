e869c9d29628f74075441f60dc8b07dd38a88f43
Admexter <mypancakes@wp.pl>
2020-06-12 14:18:45 +0200

    Change download to use lseek

--- download.cpp
+++ download.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <cstring>
+#include <climits>
 
 #include "mynfs.h"
 
@@ -12,21 +13,46 @@
 	}\
 }
 
+int write_all(int sock, const char *buf, int size) {
+	int left = size;
+	while (left > 0) {
+		int n = write(sock, buf, left);
+		if (n == 0) return -1;
+		if (n < 0) {
+			std::cout << "write_all error: " << strerror(errno) << std::endl;
+			return -1;
+		}
+		left -= n;
+		buf += n;
+	}
+	return 0;
+}
+
 using namespace std;
 
-// Usage: download host login pass remote_from local_to
+// Usage: download host login pass remote_from local_to (bytes_start bytes_amount)
 int main(int argc, const char **argv) {
 	if (argc < 6) return -1;
+	unsigned long start = 0;
+	unsigned long amount = INT_MAX;
+	if (argc >= 7) {
+		start = atol(argv[6]);
+		amount = atol(argv[7]);
+		cout << "Seeking to " << start << " and reading " << amount << " bytes" << endl;
+	}
 	mynfs_connection *conn;
 	try_op(mynfs_connect(&conn, argv[1], argv[2], argv[3]), "connect failed");
 	int remote;
 	try_op((remote = mynfs_open(conn, argv[4], OF_RDONLY, 0)), "open failed");
-	int local = open(argv[5], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	try_op(mynfs_lseek(conn, remote, start, SEEK_SET), "lseek failed");
+	int local = open(argv[5], O_WRONLY | O_CREAT, 0777);
 	try_op(local, strerror(errno));
+	try_op(lseek(local, start, SEEK_SET), "lseek failed");
 	char buf[256] = {0};
 	int n;
-	while((n = mynfs_read(conn, remote, buf, sizeof(buf))) > 0) {
-		write(local, buf, n);
+	while(amount > 0 && (n = mynfs_read(conn, remote, buf, min(sizeof(buf), amount))) > 0) {
+		write_all(local, buf, n);
+		amount -= n;
 	}
 	try_op(n, "read failed");
 	close(local);
